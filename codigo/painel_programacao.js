bfdbb0f615c975bb4c855df946895e4ef207d62c
Reinan-Alves <reinanalves1988@gmail.com>
2022-01-02 12:12:53 -0300

    movendo o codigo com as funcões das casas para o arquivo de codigo do tabuleiro

--- codigo/painel_programacao.js
+++ codigo/painel_programacao.js
@@ -1,474 +1,152 @@
 // criando a classe dos jogadores com suas posiveis ações
 
 class jogador {
-      nome;
-      saldo = 1045;
+  nome;
+  saldo = 1045;
 
-      constructor(nome) {
-      this.nome = nome; 
+  constructor(nome) {
+    this.nome = nome;
+  }
+
+  //depositar valor na conta
+
+  depositar(valor) {
+    if (valor < 0) {
+      alert("não é possivel efetuar transações com valores negativo");
+      return;
+    }
+    if (this.saldo <= 0) {
+      alert("este jogador esta fora");
+      return;
+    } else {
+      this.saldo += valor;
+    }
+  }
+
+  //transação valida
+
+  //retirar valor da conta
+
+  sacar(valor) {
+    if (valor < 0) {
+      alert("não é possivel efetuar transações com valores negativo");
+      return;
     }
 
-//depositar valor na conta
-
-    depositar(valor){
-    if(valor < 0){
-        alert('não é possivel efetuar transações com valores negativo')
-        return;
-    }
-    if(this.saldo <= 0){
-     alert('este jogador esta fora')
-     return;
-    }else{
-    this.saldo += valor;
+    if (this.saldo <= 0) {
+      alert("este jogador esta fora");
+      return;
     }
 
-}
-
-//transação valida
-
-
-//retirar valor da conta
-
-    sacar(valor){
-
-        if(valor < 0){
-            alert('não é possivel efetuar transações com valores negativo')
-            return;
-        }
-
-        if(this.saldo <= 0){
-            alert('este jogador esta fora')
-            return;
-           }
-           
-        if(this.saldo < valor){
-            alert('saldo insuficiente,escolha um valor menor') 
-            return;
-         }else{
-            this.saldo -= valor;
-            return valor
-            }
-
-}
-
-//tranferir
-
-transferir(jogador, valor){
-
-    if(jogador.saldo <= 0){
-        alert('jogador fora, a conta ja está desativada')
-        return;
-    }else if(this.saldo < valor){
-    alert('saldo insuficiente,escolha um valor menor')
-    return;
-    }else{
-        this.sacar(valor)
-        jogador.depositar(valor)
+    if (this.saldo < valor) {
+      alert("saldo insuficiente,escolha um valor menor");
+      return;
+    } else {
+      this.saldo -= valor;
+      return valor;
     }
-      
+  }
+
+  //tranferir
+
+  transferir(jogador, valor) {
+    if (jogador.saldo <= 0) {
+      alert("jogador fora, a conta ja está desativada");
+      return;
+    } else if (this.saldo < valor) {
+      alert("saldo insuficiente,escolha um valor menor");
+      return;
+    } else {
+      this.sacar(valor);
+      jogador.depositar(valor);
+    }
+  }
 }
-
-}
-
-
-
-
-
-
 
 // criando jogadores
 
 var contador = 1;
-var conta = []
+var conta = [];
 
+var btnAdicionar = document.querySelector("#adicionar-jogador");
+btnAdicionar.addEventListener("click", function () {
+  event.preventDefault();
 
-
-var btnAdicionar = document.querySelector("#adicionar-jogador")
-btnAdicionar.addEventListener("click",function(){
-    
-    event.preventDefault();
-
-    let nomeJogadortxt = document.querySelector("#nome")
-    let nomeJogador = nomeJogadortxt.value;
-    let jogadorCriado = new jogador(nomeJogador);
-        conta[contador] = jogadorCriado
-    let jogadoresCriados = document.querySelector("#jogadores")
-        jogadoresCriados.innerHTML +=`conta${contador}: ${conta[contador].nome},Saldo inicial R$: ${conta[contador].saldo}<br>`;
-        contador++
-        
+  let nomeJogadortxt = document.querySelector("#nome");
+  let nomeJogador = nomeJogadortxt.value;
+  let jogadorCriado = new jogador(nomeJogador);
+  conta[contador] = jogadorCriado;
+  let jogadoresCriados = document.querySelector("#jogadores");
+  jogadoresCriados.innerHTML += `conta${contador}: ${conta[contador].nome},Saldo inicial R$: ${conta[contador].saldo}<br>`;
+  contador++;
 });
 
-    //var status =  document.querySelector("#statusDoJogo")
+//var status =  document.querySelector("#statusDoJogo")
 
 // ação de Deposito no jogo
 
-var botaoDeDeposito = document.querySelector("#btnDepositar")
-botaoDeDeposito.addEventListener("click", function(){
+var botaoDeDeposito = document.querySelector("#btnDepositar");
+botaoDeDeposito.addEventListener("click", function () {
+  event.preventDefault();
 
-event.preventDefault();
+  var valorinputDeposito = document.querySelector("#inputDeposito");
+  var depositoDigitado = Number(valorinputDeposito.value);
+  var inputContaDeposito = document.querySelector("#inputContaDeposito");
+  var nContaDep = inputContaDeposito.value;
+  var ultimamovimentacao = document.querySelector("#ultimaMovimentacao");
+  conta[nContaDep].depositar(depositoDigitado);
+  ultimamovimentacao.innerHTML = `Depósito de R$${depositoDigitado} para ${conta[nContaDep].nome} `;
 
-
-var valorinputDeposito = document.querySelector("#inputDeposito");
-var depositoDigitado = Number(valorinputDeposito.value);
-var inputContaDeposito = document.querySelector("#inputContaDeposito");
-var nContaDep = inputContaDeposito.value
-var ultimamovimentacao = document.querySelector("#ultimaMovimentacao");
-    conta[nContaDep].depositar(depositoDigitado)
-    ultimamovimentacao.innerHTML = `Depósito de R$${depositoDigitado} para ${conta[nContaDep].nome} `
-   
-    atualizarStatus();
-})
-
-
-
-
-
+  atualizarStatus();
+});
 
 // ação de saque no jogo
-var botaoDeSaque = document.querySelector("#btnSacar")
-botaoDeSaque.addEventListener("click", function(){
+var botaoDeSaque = document.querySelector("#btnSacar");
+botaoDeSaque.addEventListener("click", function () {
+  event.preventDefault();
 
-event.preventDefault();
+  var valorinputsaque = document.querySelector("#inputSaque");
+  var saqueDigitado = Number(valorinputsaque.value);
+  var inputContaSaque = document.querySelector("#inputContaSaque");
+  var nContaSaq = inputContaSaque.value;
+  var ultimamovimentacao = document.querySelector("#ultimaMovimentacao");
+  conta[nContaSaq].sacar(saqueDigitado);
+  ultimamovimentacao.innerHTML = `Retirada de R$${saqueDigitado} de ${conta[nContaSaq].nome} `;
 
-
-var valorinputsaque = document.querySelector("#inputSaque");
-var saqueDigitado = Number(valorinputsaque.value);
-var inputContaSaque = document.querySelector("#inputContaSaque");
-var nContaSaq = inputContaSaque.value
-var ultimamovimentacao = document.querySelector("#ultimaMovimentacao");
-    conta[nContaSaq].sacar(saqueDigitado)
-    ultimamovimentacao.innerHTML = `Retirada de R$${saqueDigitado} de ${conta[nContaSaq].nome} `
-
-    atualizarStatus();
-})
-
-
+  atualizarStatus();
+});
 
 // ação de tranferencia no jogo
-var btnTransferir = document.querySelector("#btnTransferir")
-btnTransferir.addEventListener("click", function(){
+var btnTransferir = document.querySelector("#btnTransferir");
+btnTransferir.addEventListener("click", function () {
+  event.preventDefault();
 
-event.preventDefault();
+  var valorinputTranferencia = document.querySelector(
+    "#inputValorDaTransferencia"
+  );
+  var tranferenciaDigitada = Number(valorinputTranferencia.value);
+  var inputContaEmissora = document.querySelector("#inputContaEmissora");
+  var nContaEmissora = inputContaEmissora.value;
+  var inputContaReceptora = document.querySelector("#inputContaReceptora");
+  var nContaReceptora = inputContaReceptora.value;
+  var ultimamovimentacao = document.querySelector("#ultimaMovimentacao");
+  conta[nContaEmissora].transferir(
+    conta[nContaReceptora],
+    tranferenciaDigitada
+  );
+  ultimamovimentacao.innerHTML = `Transferência de R$ ${tranferenciaDigitada} de ${conta[nContaEmissora].nome} para ${conta[nContaReceptora].nome}`;
 
-var valorinputTranferencia = document.querySelector("#inputValorDaTransferencia");
-var tranferenciaDigitada = Number( valorinputTranferencia.value);
-var inputContaEmissora = document.querySelector("#inputContaEmissora");
-var nContaEmissora = inputContaEmissora.value
-var inputContaReceptora = document.querySelector("#inputContaReceptora");
-var nContaReceptora = inputContaReceptora.value
-var ultimamovimentacao = document.querySelector("#ultimaMovimentacao");
-conta[nContaEmissora].transferir(conta[nContaReceptora], tranferenciaDigitada)
-ultimamovimentacao.innerHTML = `Transferência de R$ ${tranferenciaDigitada} de ${conta[nContaEmissora].nome} para ${conta[nContaReceptora].nome}`
+  atualizarStatus();
+});
 
-atualizarStatus();
-})
-
-
-function atualizarStatus(){
-    var status =  document.querySelector("#statusDoJogo")
-    status.innerHTML ="";
-    for(let i = 1;i <= contador;i++){
+function atualizarStatus() {
+  var status = document.querySelector("#statusDoJogo");
+  status.innerHTML = "";
+  for (let i = 1; i <= contador; i++) {
     status.innerHTML += `Saldo de ${conta[i].nome} R$ ${conta[i].saldo}<br>`;
-    
-    if(conta[i].saldo <= 0){
 
-     status.innerHTML += `${conta[i].nome} Quebrou e está fora<br>`   
-
+    if (conta[i].saldo <= 0) {
+      status.innerHTML += `${conta[i].nome} Quebrou e está fora<br>`;
     }
-
-    }
+  }
 }
-
-// funções das casas
-
-
-var btn1 = document.querySelector("#casa1")
-btn1.addEventListener("click", function(){
-  
-  alert("Hora de receber seu o salário suado, deposito de R$1045,00")
-});
-
-var btn2 = document.querySelector("#casa2")
-btn2.addEventListener("click", function(){
-    alert("Hoje é dia de fazer Supermercado, saque R$300,00")
-});
-
-var btn3 = document.querySelector("#casa3")
-btn3.addEventListener("click", function(){
-    alert("Parabéns! É seu aniversário, receba R$50,00 de cada jogador")
-});
-
-var btn4 = document.querySelector("#casa4")
-btn4.addEventListener("click", function(){
-    alert("Oh,nao! você foi assaltado,levaram metade do seu dinhero")
-});
-
-var btn5 = document.querySelector("#casa5")
-btn5.addEventListener("click", function(){
-    alert("Alguém adoeçeu,vá até a farmácia e compre alguns remédios, saque R$100,00")
-});
-
-var btn6 = document.querySelector("#casa6")
-btn6.addEventListener("click", function(){
-    alert("Seu gás acabou, saque 100 reais e Ligue pro gás.")
-});
-
-var btn7 = document.querySelector("#casa7")
-btn7.addEventListener("click", function(){
-    alert("Receba seus R$ 50,00 de hora extras trabalhadas")
-});
-
-var btn8 = document.querySelector("#casa8")
-btn8.addEventListener("click", function(){
-    alert("você pegou dinheiro emprestado com alguem, não lembra? escolha um jogador e pague sua divida de R$ 150,00")
-});
-
-var btn9 = document.querySelector("#casa9")
-btn9.addEventListener("click", function(){
-    alert("chegou um presentinho chamado talão de água saque R$ 150,00 e pague esta conta")
-});
-
-var btn10 = document.querySelector("#casa10")
-btn10.addEventListener("click", function(){
-    alert("Parabéns! você conseguio vender aquele movél velho na olx, receba o depósito de R$50,00")
-});
-
-var btn11 = document.querySelector("#casa11")
-btn11.addEventListener("click", function(){
-    alert("É melhor pagar a conta de Luz antes de cortem,saque R$150,00")
-});
-
-var btn12 = document.querySelector("#casa12")
-btn12.addEventListener("click", function(){
-    alert("Chegou a triste hora de pagar o aluguel,saque R$250,00")
-});
-
-var btn13 = document.querySelector("#casa13")
-btn13.addEventListener("click", function(){
-    alert("Para continuar no,instagram,youtube e netflix,saque R$60,00 e pague o boleto da internet")
-});
-
-var btn14 = document.querySelector("#casa14")
-btn14.addEventListener("click", function(){
-    alert("Espero que tenha se planejado bem, seu FGTS está liberado, receba R$300,00")
-});
-
-var btn15 = document.querySelector("#casa15")
-btn15.addEventListener("click", function(){
-    alert("Voce cobriu um amigo em um dia de trabalho, receba R$100,00")
-});
-
-var btn16 = document.querySelector("#casa16")
-btn16.addEventListener("click", function(){
-    alert("Voce precisou faltar ao trabalho e um amigo te cobriu,saque R$100,00")
-});
-
-var btn17 = document.querySelector("#casa17")
-btn17.addEventListener("click", function(){
-    alert("Um dos jogadores te deve, escolha um jogador e cobre uma transferência de R$100,00")
-});
-
-var btn18 = document.querySelector("#casa18")
-btn18.addEventListener("click", function(){
-    alert("Oba! voce ganhou um bônus de R$100,00 da sua empresa")
-});
-
-var btn19 = document.querySelector("#casa19")
-btn19.addEventListener("click", function(){
-    alert("Saque R$50,00, para vc pagar seu transporte")
-});
-
-var btn20 = document.querySelector("#casa20")
-btn20.addEventListener("click", function(){
-    alert("Seu dia de sorte vc acaba de ganhar R$100,00 no esporte Net")
-});
-
-var btn21 = document.querySelector("#casa21")
-btn21.addEventListener("click", function(){
-    alert("Seja bondoso e faça uma doação de R$100,00 para o jogagor que tiver o menor saldo, se este jogador for voçe,apenas guarde seu dinheiro.")
-
-});
-
-var btn22 = document.querySelector("#casa22")
-btn22.addEventListener("click", function(){
-    alert("Se atrasou e não vai de busão? saque R$50,00 e pegue um Uber")
-
-});
-
-var btn23 = document.querySelector("#casa23")
-btn23.addEventListener("click", function(){
-    alert("Voce apostou na loteria!...mas nao ganhou(kkk) saque R$50,00")
-
-});
-
-var btn24 = document.querySelector("#casa24")
-btn24.addEventListener("click", function(){
-    alert("seu auxilio emergêncial de R$375,00 está liberado")
-
-});
-
-var btn25 = document.querySelector("#casa25")
-btn25.addEventListener("click", function(){
-    alert("Aquela saida com seus amigos te custou R$100,00")
-
-});
-
-
-var btn26 = document.querySelector("#casa26")
-btn26.addEventListener("click", function(){
-    alert("Que sorte você achou R$10,00 no chão")
-
-});
-
-var btn27 = document.querySelector("#casa27")
-btn27.addEventListener("click", function(){
-    alert("você perdeu sua carteira com R$50,00 dentro")
-
-});
-
-var btn28 = document.querySelector("#casa28")
-btn28.addEventListener("click", function(){
-    alert("A lâmpada da sala queimou,saque R$25,00")
-
-});
-
-var btn29 = document.querySelector("#casa29")
-btn29.addEventListener("click", function(){
-    alert("Se quer dá uma de chique e sair de taxi, saque R$100,00")
-
-});
-
-var btn30 = document.querySelector("#casa30")
-btn30.addEventListener("click", function(){
-    alert("Dia de repouso!!!,nem gaste,nem ganhe")
-
-});
-
-var btn31 = document.querySelector("#casa31")
-btn31.addEventListener("click", function(){
-    alert("Ricos cada vez mais ricos...se você não for o mais rico do jogo,pague R$ 200,00 a ele ou a eles caso tenha mais de um com o mesmo saldo")
-});
-
-var btn32 = document.querySelector("#casa32")
-btn32.addEventListener("click", function(){
-    alert("Os humildes são os mais bondosos..se vc for o mais pobre do jogo, doe R$ 50,00 para um jogador de sua escolha")
-
-});
-
-
-var btn33 = document.querySelector("#casa33")
-btn33.addEventListener("click", function(){
-    alert("Sua tv queimou, saque R$300,00 e leve para consertar")
-
-});
-
-
-var btn34 = document.querySelector("#casa34")
-btn34.addEventListener("click", function(){
-    alert("Voce perdeu seu cartão de credito com a senha anotada e fizeram uma compra de R$ 200,00")
-
-});
-
-
-var btn35 = document.querySelector("#casa35")
-btn35.addEventListener("click", function(){
-    alert("Voce  recebeu um pix acidental de R$ 50,00")
-
-});
-
-
-var btn36 = document.querySelector("#casa36")
-btn36.addEventListener("click", function(){
-    alert("se voce for o mais pobre do jogo receba R$ 200,00 do mais rico")
-
-});
-
-var btn37 = document.querySelector("#casa37")
-btn37.addEventListener("click", function(){
-    alert("Os jogadores que estão entre as casas 01 e 10 devem te pagar R$ 50,00")
-
-});
-
-var btn38 = document.querySelector("#casa38")
-btn38.addEventListener("click", function(){
-    alert("Os jogadores que estão entre as casas 10 e 20 devem te pagar R$ 50,00")
-
-});
-
-
-var btn39 = document.querySelector("#casa39")
-btn39.addEventListener("click", function(){
-    alert("Os jogadores que estão entre as casas 20 e 30 devem te pagar R$ 50,00")
-
-});
-
-var btn40 = document.querySelector("#casa40")
-btn40.addEventListener("click", function(){
-    alert("Voce deve pagar R$50 a cada jogador")
-
-});
-
-var btn41 = document.querySelector("#casa41")
-btn41.addEventListener("click", function(){
-    alert("Escolha um jogador e transfira metade do valor de sua conta para ele")
-
-});
-
-var btn42 = document.querySelector("#casa42")
-btn42.addEventListener("click", function(){
-    alert("Escolha um jogador para transferir metade do valor da conta dele pata sua conta")
-
-});
-
-var btn43 = document.querySelector("#casa43")
-btn43.addEventListener("click", function(){
-    alert("Dia de Netflix com lanchinho do ifood, saque R$50,00")
-
-});
-
-
-var btn44 = document.querySelector("#casa44")
-btn44.addEventListener("click", function(){
-    alert("Hoje voce precisou ir ao dentista e gastou R$150,00")
-
-});
-
-
-var btn45 = document.querySelector("#casa45")
-btn45.addEventListener("click", function(){
-    alert("Foi concluido uma causa na justiça que vc tinha e vc ganhou R$2000,00")
-
-});
-
-
-var btn46 = document.querySelector("#casa46")
-btn46.addEventListener("click", function(){
-    alert("Voce saiu pra se divertir no final de semana e gastou R$200,00")
-
-});
-
-var btn47 = document.querySelector("#casa47")
-btn47.addEventListener("click", function(){
-    alert("Nossa,voce tranferiu um pix para conta errada e perdeu R$50,00")
-
-});
-
-
-var btn48 = document.querySelector("#casa48")
-btn48.addEventListener("click", function(){
-    alert("Voce fez um chá de casa nova e todos os jogadores te pagam R$50,00")
-
-});
-
-var btn49 = document.querySelector("#casa49")
-btn49.addEventListener("click", function(){
-    alert("Uma senhora que precisava atravessar a rua e voce ajudou te deu R$20,00")
-
-});
-
-
-var btn50 = document.querySelector("#casa50")
-btn50.addEventListener("click", function(){
-    alert("Amigo é coisa pra se guardar...escolha alguem e empreste R$100,00")
-
-});
