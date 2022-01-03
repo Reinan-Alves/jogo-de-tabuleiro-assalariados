5e898df784f1b8b6f04ac72e9ea852b58442130a
Reinan-Alves <reinanalves1988@gmail.com>
2022-01-03 10:38:06 -0300

    organizando ordem entre o spinner e os dados

--- codigo/dado.js
+++ codigo/dado.js
@@ -2,30 +2,30 @@ var espacoDados = document.getElementById("espaco-dados");
 
 espacoDados.addEventListener("click", function () {
   let face = Math.floor(Math.random() * 6) + 1;
-  $("#spinner").toggle();
   $("#dado").toggle();
+  $("#spinner").toggle();
 
+  switch (face) {
+    case 1:
+      document.getElementById("dado").src = "estilo/imagens/face1.png";
+      break;
+    case 2:
+      document.getElementById("dado").src = "estilo/imagens/face2.png";
+      break;
+    case 3:
+      document.getElementById("dado").src = "estilo/imagens/face3.png";
+      break;
+    case 4:
+      document.getElementById("dado").src = "estilo/imagens/face4.png";
+      break;
+    case 5:
+      document.getElementById("dado").src = "estilo/imagens/face5.png";
+      break;
+    case 6:
+      document.getElementById("dado").src = "estilo/imagens/face6.png";
+      break;
+  }
   setTimeout(function () {
-    switch (face) {
-      case 1:
-        document.getElementById("dado").src = "estilo/imagens/face1.png";
-        break;
-      case 2:
-        document.getElementById("dado").src = "estilo/imagens/face2.png";
-        break;
-      case 3:
-        document.getElementById("dado").src = "estilo/imagens/face3.png";
-        break;
-      case 4:
-        document.getElementById("dado").src = "estilo/imagens/face4.png";
-        break;
-      case 5:
-        document.getElementById("dado").src = "estilo/imagens/face5.png";
-        break;
-      case 6:
-        document.getElementById("dado").src = "estilo/imagens/face6.png";
-        break;
-    }
     $("#spinner").toggle();
     $("#dado").toggle();
   }, 2000);
